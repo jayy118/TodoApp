26c0f8a2339b3762ff2e98dac5b4d3fd21366b20
= <=>
2023-03-22 13:10:55 +0900

    3/22 수업(2.2절 완료)

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,23 +1,30 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TodoDTO;
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("todo")
 public class TodoController {
-    @GetMapping("/testTodo")
+
+    @Autowired
+    private TodoService service;
+    @GetMapping("/test")
     public ResponseEntity<?> testTodo(){
 
-        TodoDTO response = TodoDTO.builder()
-                .id("123")
-                .title("이재현")
-                .done(true)
-                .build();
-
+        String str = service.testService();
+        List<String> list = new ArrayList<>();
+        list.add(str);
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
         return ResponseEntity.ok().body(response);
     }
 }
